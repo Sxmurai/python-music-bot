60fc30285c4fa17f87792429fee066866b33cabe
Sxmurai <gavrw0607@gmail.com>
2021-01-26 15:34:47 -0600

    fix cog loading and music shit

--- main.py
+++ main.py
@@ -13,32 +13,33 @@ cwd = str(cwd)
 config = load_config("config.json")
 
 bot = Bot(
-    command_prefix=config["prefix"],
-    case_insensitive=True,
-    help_command=DefaultHelpCommand(),
-    owner_ids=config["owner"],
-    description="A Python music bot"
+  command_prefix=config["prefix"],
+  case_insensitive=True,
+  help_command=DefaultHelpCommand(),
+  owner_ids=config["owner"],
+  description="A Python music bot"
 )
 
 bot.config = config
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name}#{bot.user.discriminator} is ready!")
+  print(f"{bot.user.name}#{bot.user.discriminator} is ready!")
 
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    await bot.process_commands(message)
-
-
-if __name__ == "__main__":
   for file in os.listdir(cwd + "\cogs"):
     if not file.startswith("_"):
       bot.load_extension(f"cogs.{file[:-3]}")
 
-  bot.run(config["token"])
+  print(f"Loaded {len(bot.extensions)} extensions")
 
+
+@bot.event
+async def on_message(message):
+  if message.author.bot:
+    return
+
+  await bot.process_commands(message)
+
+
+if __name__ == "__main__":
+  bot.run(config["token"])
